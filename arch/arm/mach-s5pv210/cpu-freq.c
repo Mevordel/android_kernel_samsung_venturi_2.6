8755e463eb7d70ebff3be70ba9b4e9d6f6a9aab0
KOala888 <koala8328@gmail.com>
2013-01-10 15:48:05 +0100

    fixes again ;x

--- arch/arm/mach-s5pv210/cpu-freq.c
+++ arch/arm/mach-s5pv210/cpu-freq.c
@@ -906,9 +906,9 @@ static int __init s5pv210_cpufreq_driver_init(struct cpufreq_policy *policy)
 
 	if (level == CPUFREQ_TABLE_END) { /* Not found */
 		pr_err("[%s:%d] clock speed does not match: "
-				"%d. Using L2 of 800MHz.\n",
+				"%d. Using L8 of 800MHz.\n",
 				__FILE__, __LINE__, rate);
-		level = L2;
+		level = L8;
 	}
 
 	backup_dmc0_reg = __raw_readl(S5P_VA_DMC0 + 0x30) & 0xFFFF;
